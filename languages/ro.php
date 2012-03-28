b6d4c909157dc7de4e951e80cd1472b1e46dcfd8
Bastian Allgeier <bastian.allgeier@gmail.com>
2012-03-28 11:04:20 +0200

    added rumanian translation

---
+++ languages/ro.php
@@ -0,0 +1,196 @@
+<?php
+
+/* 
+ * author: Seitenwerk <http://seitenwerk.at> & CIUCIU <http://ciuciu.at>
+ */
+
+l::set(array(
+  
+  // global 
+  'ok' => 'Ok',
+  'cancel' => 'Anulează',
+  'nochanges' => 'Nu a fost schimbat nimic',
+
+  // header
+  'header.title' => 'Kirby',
+  'header.logout' => 'Deconectează', 
+  
+  // subheader
+  'subheader.home' => 'Acasă',  
+  'subheader.site' => 'Pagina ta',
+  'subheader.info' => 'Informaţii',
+
+  // tabs
+  'tabs.overview' => 'Ansamblu',
+  'tabs.info' => 'Editează Informaţiile',
+  'tabs.content' => 'Conţinut',
+  'tabs.files' => 'Documente',
+  'tabs.options' => 'URL',
+  'tabs.preview' => 'Previzionare',
+
+  // dashboard
+  'dashboard.edit' => 'Editează',
+  'dashboard.content' => 'Conţinut',
+  'dashboard.content.empty' => 'Această pagină nu are conţinut',
+  'dashboard.files' => 'Documente',
+  'dashboard.template' => 'Model',
+  'dashboard.stats.images' => 'Poze', 
+  'dashboard.stats.videos' => 'Videouri', 
+  'dashboard.stats.docs' => 'Documente', 
+  'dashboard.stats.sound' => 'Audio', 
+  'dashboard.stats.others' => 'Diverse', 
+
+  // home
+  'home.siteinfo' => 'Informaţii pagină',
+  'home.edit' => 'Editează',
+  'home.warning.title' => 'Vă rugăm să eliminaţi eroarea întâi',
+  'home.warning.permissions.title' => 'Permisiuni',
+  'home.warning.permissions.text' => 'Permisiunile din registrul de conţinut nu sunt setate corect. Vă rugăm să scimbaţi permisiunile pentru toate dosarele în <strong>0755</strong> şi pentru toate fişierele în <strong>0666</strong>',
+  'home.warning.blueprints.title' => 'Blueprints',
+  'home.warning.blueprints.text' => 'Nu există blueprints disponibile pentru modelele următoares<br /> Citeşte mai multe despre <a href="http://getkirby.com/panel/blueprints">cum să instezi blueprints pentru modele</a>',
+  'home.warning.thumbs.title' => 'Thumbnails',
+  'home.warning.thumbs.text' => 'Vă rugăm să adăugaţi un <strong>thumbs</strong> folder in directorul principal al site-ului dvs. şi să modificaţi permisiunile în <strong>0755</strong>',
+  
+  // siteinfo
+  'siteinfo.button' => 'Salvează',
+  'siteinfo.errors.title' => 'Vă rugăm să adăugaţi un titlu',
+  'siteinfo.errors.permissions' => '<strong>content/site.txt</strong> nu este editabil. <br />Vă rugăm să schimbaţi permisiunile şi să încercaţi din nou.',
+  
+  // content form 
+  'content.save' => 'Salvează',
+  'content.previous' => 'Înapoi',
+  'content.next' => 'Înainte',
+  'content.invisible' => 'Invizibil',
+  
+  // no content available
+  'nocontent.title' => 'Nici un conţinut',
+  'nocontent.text' => 'Nu există conţinut pentru această pagină.',
+    
+  // pages
+  'pages.title' => 'Pagini vizibile',
+  'pages.title.home' => 'Pagini',
+  'pages.home' => 'Acasă',
+  'pages.sort' => 'Sortează',
+  'pages.add' => 'Adaugă',
+  'pages.delete' => 'Şterge',
+  'pages.invisible' => 'Subpagini invizibile',
+  'pages.invisible.home' => 'Pagini invizibile',
+  'pages.no.visible' => 'Nici o pagină vizibilă până acum',
+  'pages.no.invisible' => 'Nici o pagină invizibilă până acum',
+  'pages.moved' => 'Pagina a fost mutată',
+  'pages.sorted' => 'Pagina a fost sortată',
+  'pages.show.more' => 'Arată mai mult',
+  'pages.show.less' => 'Arată mai puţin',
+  'pages.errors.notfound' => 'Pagina nu a putut fi găsită',
+  'pages.errors.move' => 'Pagina nu a putut fi mutată',
+  'pages.errors.sort' => 'Nu toate paginile au putut fi sortate',
+  'pages.errors.nosort' => 'Nu e nimic de sortat',
+
+  // no subpages allowed
+  'nopages.title' => 'Subpagini',
+  'nopages.text' => 'Această pagină nu ar trebui să aibă subpagini',  
+  
+  // add page
+  'pages.add.title' => 'Adaugă o pagină nouă',
+  'pages.add.label.title' => 'Titlu',
+  'pages.add.label.url' => 'URL',
+  'pages.add.label.template' => 'Model',
+  'pages.add.button' => 'Salvează',
+  'pages.add.errors.title' => 'Vă rugăm să adăugaţi un titlu',
+  'pages.add.errors.url' => 'Vă rugăm să adăugaţi o adresă URL pentru pagina dvs.',
+  'pages.add.errors.characters' => 'Vă rugăm să folosiţi numai următoarele caractere a-z, 0-9, _, - pentru adresa URL',
+  'pages.add.errors.exists' => 'O pagina cu această adresă URL există deja',
+  'pages.add.errors.permissions' => 'Folderul nu a putut fi creat',
+  'pages.add.success' => 'Pagina a fost creată',
+
+  // update page
+  'pages.update.errors.title' => 'Vă rugăm să adăugaţi un titlu',
+    
+  // delete page  
+  'pages.delete.title' => 'Şterge această pagină',
+  'pages.delete.button' => 'Şterge',
+  'pages.delete.errors.homepage' => 'Nu puteţi şterge pagina de start',
+  'pages.delete.errors.errorpage' => 'Nu puteţi şterge pagina de eroare',
+  'pages.delete.errors.subpages' => 'Această pagină are subpagini. Vă rugăm să le ştergeţi mai întâi.',
+  'pages.delete.errors.permissions' => 'Pagina nu a putut fi eliminată',
+  'pages.delete.success' => 'Pagina a fost eliminată',
+
+  // options
+  'options.url' => 'URL',
+  'options.button' => 'Salvează',
+  'options.home.url' => 'URL',
+  'options.home.text' => 'Nu puteţi schimba URL-ul de pagina de start',
+  'options.error.text' => 'Nu puteţi schimba URL-ul paginii de eroare',
+  'options.errors.homepage' => 'Nu puteţi schimba URL-ul de pagina de start',
+  'options.errors.errorpage' => 'Nu puteţi schimba URL-ul paginii de eroare',
+  'options.errors.characters' => 'Vă rugăm să folosiţi numai următoarele caractere a-z, 0-9, _, -',
+  'options.errors.permissions' => 'Folderul de conţinut nu poate fi mutat',
+  'options.errors.exists' => 'Noua adresă URL există deja',
+  'options.errors.move' => 'Folderul de conţinut nu poate fi mutat',
+
+  // files
+  'files.title' => 'Fişiere',
+  'files.upload' => 'Încărcaţi un fişier nou',
+  'files.edit' => 'Editează',
+  'files.replace' => 'înlocuieşte',
+  'files.delete' => 'Şterge',
+  'files.empty' => 'Nici un fişier până acum',
+
+  'nofiles.title' => 'Nici un fişier',
+  'nofiles.text' => 'Această pagină nu ar trebui să aibă fişiere',
+  
+  // delete file
+  'files.delete.title' => 'Ştergeţi acest fişier',
+  'files.delete.button' => 'Şterge',
+  'files.delete.errors.notfound' => 'Fişierul nu a putut fi găsit',
+  'files.delete.errors.permissions' => 'Fişierul nu a putut fi şters',
+  'files.delete.success' => 'Fişierul a fost şters',
+
+  // edit file
+  'files.edit.title' => 'Editare fişier',
+  'files.edit.filename' => 'Numele fisierului (fara extensie)',
+  'files.edit.button' => 'Salvează',
+  'files.edit.errors.notfound' => 'Fişierul nu a putut fi găsit',
+  'files.edit.errors.exists' => 'Noul nume există deja',
+  'files.edit.errors.permissions' => 'Fişierul nu a putut fi redenumit',
+  'files.edit.success' => 'Fişierul a fost salvat',
+
+  // replace file
+  'files.replace.title' => 'Înlocuiţi acest fişier',
+  'files.replace.file' => 'Fişier',
+  'files.replace.choose' => 'Vă rugăm să alegeţi un fişier nou din computer...',
+  'files.replace.button' => 'Înlocuieşte',
+  'files.replace.errors.notfound' => 'Fişierul nu a putut fi găsit',
+  'files.replace.success' => 'Fişierul a fost înlocuit',
+
+  // upload file
+  'files.upload.title' => 'Încărcaţi un fişier nou',
+  'files.upload.choose' => 'Vă rugăm să alegeţi un fişier nou din computer...',
+  'files.upload.button' => 'Încarcă',
+      
+  // login  
+  'login.button' => 'Autentificare',
+  'login.username' => 'Nume',
+  'login.password' => 'Parolă',
+  'login.error' => 'Numele sau parola incorectă',
+  'login.success' => 'Aţi fost conectat',
+  'login.error.browser.title' => 'Folosiţi un browser vechi',
+  'login.error.browser.text' => 'Vă rugăm să faceţi upgrade la ultima versiune de la Google Chrome, Firefox, Safari or Internet Explorer.',
+  
+  // upload
+  'upload.errors.missing-file' => 'Fişierul nu a fost găsit',
+  'upload.errors.file-exists' => 'Fişierul există şi nu poate fi suprascris',
+  'upload.errors.invalid-upload' => 'Încărcarea a eşuat',
+  'upload.errors.too-big' => 'Fişierul este prea mare',
+  'upload.errors.invalid-file' => 'Acest tip de fişier nu este permis',
+  'upload.errors.move-error' => 'Fişierul nu a putut fi mutat pe server',
+  'upload.success' => 'Fişierul a fost încărcat',
+
+  // writer
+  'writer.errors.permissions' => 'Fişierul nu poate fi scris',
+  'writer.errors.write' => 'Datele nu au putut fi adăugate',
+  'writer.success' => 'Fişierul a fost salvat',
+  
+));
+
+?>
--- languages/tr.php
+++ languages/tr.php
@@ -5,191 +5,191 @@
  */
 
 l::set(array(
-  
-  // global 
-  'ok' => 'Tamam',
-  'cancel' => 'yarida birakmak',
-  'nochanges' => 'degismez',
+  
+  // global 
+  'ok' => 'Tamam',
+  'cancel' => 'yarida birakmak',
+  'nochanges' => 'degismez',
 
-  // header
-  'header.title' => 'Kirby',
-  'header.logout' => 'cikis', 
-  
-  // subheader
-  'subheader.home' => 'anasayfa',  
-  'subheader.site' => 'sayfaniz',
-  'subheader.info' => 'info',
+  // header
+  'header.title' => 'Kirby',
+  'header.logout' => 'cikis', 
+  
+  // subheader
+  'subheader.home' => 'anasayfa',  
+  'subheader.site' => 'sayfaniz',
+  'subheader.info' => 'info',
 
-  // tabs
-  'tabs.overview' => 'özet',
-  'tabs.info' => 'info uyarlamak',
-  'tabs.content' => 'icerik',
-  'tabs.files' => 'dosyalar',
-  'tabs.options' => 'URL',
-  'tabs.preview' => 'gelecek program',
+  // tabs
+  'tabs.overview' => 'özet',
+  'tabs.info' => 'info uyarlamak',
+  'tabs.content' => 'icerik',
+  'tabs.files' => 'dosyalar',
+  'tabs.options' => 'URL',
+  'tabs.preview' => 'gelecek program',
 
-  // dashboard
-  'dashboard.edit' => 'uyarlamak',
-  'dashboard.content' => 'icerik',
-  'dashboard.content.empty' => 'bos',
-  'dashboard.files' => 'dosyalar',
-  'dashboard.template' => 'tasari',
-  'dashboard.stats.images' => 'resimler', 
-  'dashboard.stats.videos' => 'videolar', 
-  'dashboard.stats.docs' => 'belgeler', 
-  'dashboard.stats.sound' => 'ses', 
-  'dashboard.stats.others' => 'baska', 
+  // dashboard
+  'dashboard.edit' => 'uyarlamak',
+  'dashboard.content' => 'icerik',
+  'dashboard.content.empty' => 'bos',
+  'dashboard.files' => 'dosyalar',
+  'dashboard.template' => 'tasari',
+  'dashboard.stats.images' => 'resimler', 
+  'dashboard.stats.videos' => 'videolar', 
+  'dashboard.stats.docs' => 'belgeler', 
+  'dashboard.stats.sound' => 'ses', 
+  'dashboard.stats.others' => 'baska', 
 
-  // home
-  'home.siteinfo' => 'sayfa info',
-  'home.edit' => 'uyarlamak',
-  'home.warning.title' => 'lüften önce onarmak!',
-  'home.warning.permissions.title' => 'giris izni',
-  'home.warning.permissions.text' => 'giris izniniz yok. Lütfen giris izniniz bütün dosyalari icin degistirin“ <br />Bitte ändern sie die Rechte für alle Ordner in <strong>0755</strong> und für alle Dateien in <strong>0666</strong>',
-  'home.warning.blueprints.title' => 'Blueprints',
-  'home.warning.blueprints.text' => 'Für folgende Templates wurden noch keine Blueprints angelegt.<br /><a href="http://getkirby.com/panel/blueprints">dosyayi okuyun nasil blueprints ayarlanir…</a>',
-  'home.warning.thumbs.title' => 'Thumbnails dosya yönetim icin',
-  'home.warning.thumbs.text' => 'Bitte legen sie einen <strong>thumbs</strong> Ordner im Hauptordner ihrer Seite an und setzen sie die Rechte auf <strong>0755</strong>',
+  // home
+  'home.siteinfo' => 'sayfa info',
+  'home.edit' => 'uyarlamak',
+  'home.warning.title' => 'lüften önce onarmak!',
+  'home.warning.permissions.title' => 'giris izni',
+  'home.warning.permissions.text' => 'giris izniniz yok. Lütfen giris izniniz bütün dosyalari icin degistirin“ <br />Bitte ändern sie die Rechte für alle Ordner in <strong>0755</strong> und für alle Dateien in <strong>0666</strong>',
+  'home.warning.blueprints.title' => 'Blueprints',
+  'home.warning.blueprints.text' => 'Für folgende Templates wurden noch keine Blueprints angelegt.<br /><a href="http://getkirby.com/panel/blueprints">dosyayi okuyun nasil blueprints ayarlanir…</a>',
+  'home.warning.thumbs.title' => 'Thumbnails dosya yönetim icin',
+  'home.warning.thumbs.text' => 'Bitte legen sie einen <strong>thumbs</strong> Ordner im Hauptordner ihrer Seite an und setzen sie die Rechte auf <strong>0755</strong>',
 
-  // siteinfo
-  'siteinfo.button' => 'kayit',
-  'siteinfo.errors.title' => 'Lütfen ünvaniniz girin',
-  'siteinfo.errors.permissions' => '<strong>content/site.txt</strong> yazilmaz <br />Lütfen giris iziniz degistirin',
+  // siteinfo
+  'siteinfo.button' => 'kayit',
+  'siteinfo.errors.title' => 'Lütfen ünvaniniz girin',
+  'siteinfo.errors.permissions' => '<strong>content/site.txt</strong> yazilmaz <br />Lütfen giris iziniz degistirin',
 
-  // content form 
-  'content.save' => 'kayit',
-  'content.previous' => 'geri',
-  'content.next' => 'ilerle',
-  'content.invisible' => 'görünmez',
-  
-  // no content available
-  'nocontent.title' => 'icerisiz',
-  'nocontent.text' => 'bu sayfa icerisiz',
-    
-  // pages
-  'pages.title' => 'alt sayfa icerikli',
-  'pages.title.home' => 'sayfa icerikli',
-  'pages.home' => 'basla sayfasi',
-  'pages.sort' => 'ayirmak',
-  'pages.add' => 'yeni sayfa',
-  'pages.delete' => 'sil',
-  'pages.invisible' => 'alt sayfa görünmez',
-  'pages.invisible.home' => 'sayfa görünmez',
-  'pages.no.visible' => 'daha icerikli sayfa yok',
-  'pages.no.invisible' => 'daha görünmez sayfa yok',
-  'pages.moved' => 'bu sayfa ertelendi',
-  'pages.sorted' => 'sayfalar ayikla',
-  'pages.show.more' => 'fazla göster',
-  'pages.show.less' => 'az göster',
-  'pages.errors.notfound' => 'sayfa bulunamadi',
-  'pages.errors.move' => 'sayfa ertelenmiyor',
-  'pages.errors.sort' => 'bütün sayfalar ayiklamiyor',
-  'pages.errors.nosort' => 'ayiklanamaz',
+  // content form 
+  'content.save' => 'kayit',
+  'content.previous' => 'geri',
+  'content.next' => 'ilerle',
+  'content.invisible' => 'görünmez',
+  
+  // no content available
+  'nocontent.title' => 'icerisiz',
+  'nocontent.text' => 'bu sayfa icerisiz',
+    
+  // pages
+  'pages.title' => 'alt sayfa icerikli',
+  'pages.title.home' => 'sayfa icerikli',
+  'pages.home' => 'basla sayfasi',
+  'pages.sort' => 'ayirmak',
+  'pages.add' => 'yeni sayfa',
+  'pages.delete' => 'sil',
+  'pages.invisible' => 'alt sayfa görünmez',
+  'pages.invisible.home' => 'sayfa görünmez',
+  'pages.no.visible' => 'daha icerikli sayfa yok',
+  'pages.no.invisible' => 'daha görünmez sayfa yok',
+  'pages.moved' => 'bu sayfa ertelendi',
+  'pages.sorted' => 'sayfalar ayikla',
+  'pages.show.more' => 'fazla göster',
+  'pages.show.less' => 'az göster',
+  'pages.errors.notfound' => 'sayfa bulunamadi',
+  'pages.errors.move' => 'sayfa ertelenmiyor',
+  'pages.errors.sort' => 'bütün sayfalar ayiklamiyor',
+  'pages.errors.nosort' => 'ayiklanamaz',
 
-  // no subpages allowed
-  'nopages.title' => 'alt sayfa',
-  'nopages.text' => 'bu sayfaya alt sayfa eklenemez',  
-  
-  // add page
-  'pages.add.title' => 'yeni sayfa ekle',
-  'pages.add.label.title' => 'ünvan',
-  'pages.add.label.url' => 'URL',
-  'pages.add.label.template' => 'Template',
-  'pages.add.button' => 'kayit',
-  'pages.add.errors.title' => 'Lütfen bir ünvan girin',
-  'pages.add.errors.url' => 'Lütfen bir URL girin',
-  'pages.add.errors.characters' => 'URL icin sadece a-z, 0-9, _, - kullanin',
-  'pages.add.errors.exists' => 'bu sayfa URL icin uygun',
-  'pages.add.errors.permissions' => 'listeyi yapamasin',
-  'pages.add.success' => 'sayfaya izin var',
+  // no subpages allowed
+  'nopages.title' => 'alt sayfa',
+  'nopages.text' => 'bu sayfaya alt sayfa eklenemez',  
+  
+  // add page
+  'pages.add.title' => 'yeni sayfa ekle',
+  'pages.add.label.title' => 'ünvan',
+  'pages.add.label.url' => 'URL',
+  'pages.add.label.template' => 'Template',
+  'pages.add.button' => 'kayit',
+  'pages.add.errors.title' => 'Lütfen bir ünvan girin',
+  'pages.add.errors.url' => 'Lütfen bir URL girin',
+  'pages.add.errors.characters' => 'URL icin sadece a-z, 0-9, _, - kullanin',
+  'pages.add.errors.exists' => 'bu sayfa URL icin uygun',
+  'pages.add.errors.permissions' => 'listeyi yapamasin',
+  'pages.add.success' => 'sayfaya izin var',
 
-  // update page
-  'pages.update.errors.title' => 'Lütfen bir ünvan girin',
-     
-  // delete page  
-  'pages.delete.title' => 'sayfayi sil',
-  'pages.delete.button' => 'sil',
-  'pages.delete.errors.homepage' => 'Homepage silinmiyor',
-  'pages.delete.errors.errorpage' => 'yanlis sayfasi silinmiyor',
-  'pages.delete.errors.subpages' => 'Bu sayfanin alt sayfasi var. Lütfen önce silin',
-  'pages.delete.errors.permissions' => 'sayfa silinmiyor',
-  'pages.delete.success' => 'sayfa silinmis',
-       
-  // options
-  'options.url' => 'URL',
-  'options.button' => 'kayit',
-  'options.home.url' => 'URL',
-  'options.home.text' => 'Homepage´in URL degistiremesin',
-  'options.error.text' => 'URL´deki yanlis sayfayi degistiremesin',
-  'options.errors.homepage' => 'Homepage´ in URL sayfasi degistiremesin',
-  'options.errors.errorpage' => 'yanlis sayfanin URL degistiremesin',
-  'options.errors.characters' => 'lütfen sadece sembolu takip edin: a-z, 0-9, _, -',
-  'options.errors.permissions' => 'dosya bu iceriklerle degismesin',
-  'options.errors.exists' => 'yeni URL var',
-  'options.errors.move' => 'iceriklerle dosya ertelenemes',
+  // update page
+  'pages.update.errors.title' => 'Lütfen bir ünvan girin',
+     
+  // delete page  
+  'pages.delete.title' => 'sayfayi sil',
+  'pages.delete.button' => 'sil',
+  'pages.delete.errors.homepage' => 'Homepage silinmiyor',
+  'pages.delete.errors.errorpage' => 'yanlis sayfasi silinmiyor',
+  'pages.delete.errors.subpages' => 'Bu sayfanin alt sayfasi var. Lütfen önce silin',
+  'pages.delete.errors.permissions' => 'sayfa silinmiyor',
+  'pages.delete.success' => 'sayfa silinmis',
+       
+  // options
+  'options.url' => 'URL',
+  'options.button' => 'kayit',
+  'options.home.url' => 'URL',
+  'options.home.text' => 'Homepage´in URL degistiremesin',
+  'options.error.text' => 'URL´deki yanlis sayfayi degistiremesin',
+  'options.errors.homepage' => 'Homepage´ in URL sayfasi degistiremesin',
+  'options.errors.errorpage' => 'yanlis sayfanin URL degistiremesin',
+  'options.errors.characters' => 'lütfen sadece sembolu takip edin: a-z, 0-9, _, -',
+  'options.errors.permissions' => 'dosya bu iceriklerle degismesin',
+  'options.errors.exists' => 'yeni URL var',
+  'options.errors.move' => 'iceriklerle dosya ertelenemes',
 
-  // files
-  'files.title' => 'dosyalar',
-  'files.upload' => 'dosyalari indir',
-  'files.edit' => 'dosylarai düzenlemek',
-  'files.replace' => 'degistirmek',
-  'files.delete' => 'sil',
-  'files.empty' => 'bu sayfa icin dosya inmedi',
+  // files
+  'files.title' => 'dosyalar',
+  'files.upload' => 'dosyalari indir',
+  'files.edit' => 'dosylarai düzenlemek',
+  'files.replace' => 'degistirmek',
+  'files.delete' => 'sil',
+  'files.empty' => 'bu sayfa icin dosya inmedi',
 
-  'nofiles.title' => 'dosya yok',
-  'nofiles.text' => 'bu sayfa icin dosya görünmez',
-  
-  // delete file
-  'files.delete.title' => 'dosyayi sil',
-  'files.delete.button' => 'sil',
-  'files.delete.errors.notfound' => 'dosya bulunmadi',
-  'files.delete.errors.permissions' => 'dosya silinmiyor',
-  'files.delete.success' => 'dosya silindi',
+  'nofiles.title' => 'dosya yok',
+  'nofiles.text' => 'bu sayfa icin dosya görünmez',
+  
+  // delete file
+  'files.delete.title' => 'dosyayi sil',
+  'files.delete.button' => 'sil',
+  'files.delete.errors.notfound' => 'dosya bulunmadi',
+  'files.delete.errors.permissions' => 'dosya silinmiyor',
+  'files.delete.success' => 'dosya silindi',
 
-  // edit file
-  'files.edit.title' => 'dosya düzenlemek',
-  'files.edit.filename' => 'dosya ismi (ilavesiz)',
-  'files.edit.button' => 'kayit',
-  'files.edit.errors.notfound' => 'dosya bulunmadi',
-  'files.edit.errors.exists' => 'dosya ismi var',
-  'files.edit.errors.permissions' => 'dosya ismi degismez',
-  'files.edit.success' => 'dosya kayitli',
+  // edit file
+  'files.edit.title' => 'dosya düzenlemek',
+  'files.edit.filename' => 'dosya ismi (ilavesiz)',
+  'files.edit.button' => 'kayit',
+  'files.edit.errors.notfound' => 'dosya bulunmadi',
+  'files.edit.errors.exists' => 'dosya ismi var',
+  'files.edit.errors.permissions' => 'dosya ismi degismez',
+  'files.edit.success' => 'dosya kayitli',
 
-  // replace file
-  'files.replace.title' => 'dosyayi degis',
-  'files.replace.file' => 'dosya',
-  'files.replace.choose' => 'bilgisayari´niza bir dosya bulun',
-  'files.replace.button' => 'degismek',
-  'files.replace.errors.notfound' => 'dosya bulunmadi',
-  'files.replace.success' => 'dosya degisti',
+  // replace file
+  'files.replace.title' => 'dosyayi degis',
+  'files.replace.file' => 'dosya',
+  'files.replace.choose' => 'bilgisayari´niza bir dosya bulun',
+  'files.replace.button' => 'degismek',
+  'files.replace.errors.notfound' => 'dosya bulunmadi',
+  'files.replace.success' => 'dosya degisti',
 
-  // upload file
-  'files.upload.title' => 'yeni dosya indir',
-  'files.upload.choose' => 'lütfen bilgisayarinizdan bir dosya acin',
-  'files.upload.button' => 'indir',
-      
-  // login  
-  'login.button' => 'kayit',
-  'login.username' => 'kullanici isim',
-  'login.password' => 'sifre',
-  'login.error' => 'giris izini yok',
-  'login.success' => 'simdi kayit oldun',
-  'login.error.browser.title' => 'eski Browser kullaniyorsunuz',
-  'login.error.browser.text' => 'lütfen Google Chrome, Firefox, Safari veya Internet Explorer güncellestirin.',
-  
-  // upload
-  'upload.errors.missing-file' => 'dosya bulunamadi',
-  'upload.errors.file-exists' => 'dosya var',
-  'upload.errors.invalid-upload' => 'upload´a hata var',
-  'upload.errors.too-big' => 'dosya cok büyük',
-  'upload.errors.invalid-file' => 'dosya tipi büyük',
-  'upload.errors.move-error' => 'dosya server´e indirilemez',
-  'upload.success' => 'dosya indirildi',
+  // upload file
+  'files.upload.title' => 'yeni dosya indir',
+  'files.upload.choose' => 'lütfen bilgisayarinizdan bir dosya acin',
+  'files.upload.button' => 'indir',
+      
+  // login  
+  'login.button' => 'kayit',
+  'login.username' => 'kullanici isim',
+  'login.password' => 'sifre',
+  'login.error' => 'giris izini yok',
+  'login.success' => 'simdi kayit oldun',
+  'login.error.browser.title' => 'eski Browser kullaniyorsunuz',
+  'login.error.browser.text' => 'lütfen Google Chrome, Firefox, Safari veya Internet Explorer güncellestirin.',
+  
+  // upload
+  'upload.errors.missing-file' => 'dosya bulunamadi',
+  'upload.errors.file-exists' => 'dosya var',
+  'upload.errors.invalid-upload' => 'upload´a hata var',
+  'upload.errors.too-big' => 'dosya cok büyük',
+  'upload.errors.invalid-file' => 'dosya tipi büyük',
+  'upload.errors.move-error' => 'dosya server´e indirilemez',
+  'upload.success' => 'dosya indirildi',
 
-  // writer
-  'writer.errors.permissions' => 'dosya yazlamaz',
-  'writer.errors.write' => 'veriler kayitlanamaz',
-  'writer.success' => 'veriler kayitlanmadi',
+  // writer
+  'writer.errors.permissions' => 'dosya yazlamaz',
+  'writer.errors.write' => 'veriler kayitlanamaz',
+  'writer.success' => 'veriler kayitlanmadi',
 
   // form buttons
   'form.buttons.h1' => 'h1',
