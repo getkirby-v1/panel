a989d12170eeb989c394087c3185711ec478b8c2
Bastian Allgeier <bastian.allgeier@gmail.com>
2012-09-05 17:56:52 +0200

    Added multi-language label support for checkbox fields

--- fields/checkbox/checkbox.php
+++ fields/checkbox/checkbox.php
@@ -1,1 +1,2 @@
+<?php $label = form::multilangtext($label, $name) ?>
 <label class="inline input"><input type="checkbox" name="<?php echo html($name) ?>"<?php echo ($value === 'on' || $value === '1') ? 'checked' : ''?>> <?php echo html($label) ?></label>
